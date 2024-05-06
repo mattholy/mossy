9f3c04eb0a054cb0e844517395e10f6aec18597c
Mattholy <smile.used@hotmail.com>
2024-05-06 15:34:07 +0800

    add dependence

--- frontend/mossy-zero/vite.config.ts
+++ frontend/mossy-zero/vite.config.ts
@@ -4,17 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     VueDevTools(),
+    VueI18nPlugin({})
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    target: 'esnext'
   }
 })
