64e8ab3809b4cf9c7bbc9f00edccf9b844977083
马特浩倪 <smile.used@hotmail.com>
2024-05-16 21:57:50 +0800

    support build vue for fastapi using 'npm run dev:build:frontend'

--- frontend/mossy-zero/vite.config.ts
+++ frontend/mossy-zero/vite.config.ts
@@ -5,36 +5,42 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { visualizer } from 'rollup-plugin-visualizer'
+import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-    VueI18nPlugin({}),
-    visualizer({ open: true })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/naive-ui')) {
-            return 'naive-ui';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          if (id.includes('src/components')) {
-            return 'components';
-          }
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueDevTools(),
+      VueI18nPlugin({}),
+      // Conditionally add the visualizer plugin
+      ...(isDev ? [visualizer({ open: true })] : [])
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      target: 'esnext',
+      outDir: isDev ? path.resolve(__dirname, '../../backend/static') : 'dist',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules/naive-ui')) {
+              return 'naive-ui';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+            if (id.includes('src/components')) {
+              return 'components';
+            }
+          },
         },
       },
-    },
+    }
   }
 })
