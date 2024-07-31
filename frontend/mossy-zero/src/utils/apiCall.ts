40dac5e50196ad12cf4a57febb9ce5d36038a71a
Mattholy <smile.used@hotmail.com>
2024-07-31 10:38:07 +0800

    nice

--- frontend/mossy-zero/src/utils/apiCall.ts
+++ frontend/mossy-zero/src/utils/apiCall.ts
@@ -1,44 +1,43 @@
-import pinia from '@/stores';
-import { useUserStateStore } from '@/stores/userStateStore';
-import type { G } from 'vitest/dist/reporters-yx5ZTtEV.js';
+import pinia from '@/stores'
+import { useUserStateStore } from '@/stores/userStateStore'
 
 interface FetchOptions {
-    endpoint: string;
-    data?: any;
-    method?: "GET" | "POST" | "PUT" | "DELETE";
+    endpoint: string
+    data?: any
+    method?: "GET" | "POST" | "PUT" | "DELETE"
 }
 
 interface MossyApiResponse {
-    status: string;
-    msg: string;
-    payload: any;
+    status: string
+    msg: string
+    payload: any
 }
 
 const baseUrl = import.meta.env.VITE_BASE_URL || ''
 
 export async function callMossyApi({ endpoint, data, method = "POST" }: FetchOptions): Promise<any> {
     if (!endpoint.startsWith('/')) {
-        throw new Error('Endpoint must start with /');
+        throw new Error('Endpoint must start with /')
     }
     if (!isValidJson(data)) {
-        throw new Error("Provided data is not a valid JSON object.");
+        throw new Error("Provided data is not a valid JSON object.")
     }
-    const userStateStore = useUserStateStore(pinia);
+    const userStateStore = useUserStateStore(pinia)
     const headers = new Headers({
         'Content-Type': 'application/json',
         'X-Mossy-API': 'api/m1',
-    });
+    })
 
     if (userStateStore.isLoggedIn) {
-        headers.append('Authorization', `Bearer ${userStateStore.currentToken}`);
+        headers.append('Authorization', `Bearer ${userStateStore.currentToken}`)
     }
 
     const fetchOptions: RequestInit = {
         method: method,
         headers: headers,
         body: JSON.stringify(data)
-    };
-    const response = await fetch(`${baseUrl}${endpoint}`, fetchOptions);
+    }
+    const response = await fetch(`${baseUrl}${endpoint}`, fetchOptions)
 
     const responseData: MossyApiResponse = await response.json()
 
@@ -51,28 +50,28 @@ export async function callMossyApi({ endpoint, data, method = "POST" }: FetchOpt
 
 function isValidJson(data: any): boolean {
     try {
-        JSON.stringify(data);
-        return true;
+        JSON.stringify(data)
+        return true
     } catch (e) {
-        return false;
+        return false
     }
 }
 
 export class MossyApiError extends Error {
-    public type: string;
-    public category: string;
-    public detail: string;
+    public type: string
+    public category: string
+    public detail: string
 
     constructor(type: string, category: string, detail: string) {
-        super(`[${type}]: ${category} - ${detail}`);
-        this.name = 'MossyApiError';
-        this.type = type;
-        this.category = category;
-        this.detail = detail;
-        Object.setPrototypeOf(this, MossyApiError.prototype);
+        super(`[${type}]: ${category} - ${detail}`)
+        this.name = 'MossyApiError'
+        this.type = type
+        this.category = category
+        this.detail = detail
+        Object.setPrototypeOf(this, MossyApiError.prototype)
     }
 
     displayError() {
-        return `Error Type: ${this.type}, Category: ${this.category}, Details: ${this.detail}`;
+        return `Error Type: ${this.type}, Category: ${this.category}, Details: ${this.detail}`
     }
 }
